d783de5165254ed356fa59639970f17e79ee3df8
Himanshi-sa <himanshisahuhs@gmail.com>
2024-12-15 08:36:04 +0000

    form compelete

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
+// html element usko target
+// with the help react-dom hme createRoot import karna hai
+// createRoot mein jo html element target kiya tha uski help se root
+// create karna hai
 
-const rootEID = ReactDOM.createRoot(document.getElementById('root'))
-const root = createRoot(rootEID)
-ReactDOM.createRoot(document.getElementById('root')).render(
+// and then we can render anything using JSX
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "@/index.css";
+import Home from "@/pages/Home";
+
+const rootEl = document.querySelector("#root");
+const root = createRoot(rootEl);
+root.render(
   <React.StrictMode>
-   <h1>Hello</h1>
-  </React.StrictMode>,
-)
+    <Home />
+  </React.StrictMode>
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "@/components/Header";
+import Aside from "@/components/Aside";
+import Form from "@/components/Form";
+
+const Home = () => {
+  return (
+    <div className="w-full p-4">
+      <Header />
+
+      <div className=" mx-auto grid max-w-7xl grid-cols-8">
+        <div className="col-span-2 hidden md:block">
+          <Aside />
+        </div>
+        <div className="col-span-full  rounded-lg p-4 md:col-span-6 md:bg-lime-400">
+          <Form />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
+
