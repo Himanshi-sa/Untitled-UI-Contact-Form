850cf08b4f65920ca1629d3861383a963cb57bf6
Himanshi-sa <himanshisahuhs@gmail.com>
2024-12-15 09:19:07 +0000

    final form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { RiSparkling2Fill, RiMagicLine } from "react-icons/ri";
 
 const services = [
   "Website Design",
@@ -13,29 +14,31 @@ const Form = () => {
   return (
     <div>
       <h1 className="w-96 text-3xl font-semibold">
-        Got Ideas We've got the skills. Let's team up.
+        Got Ideas?{" "}
+        <RiMagicLine className="inline-block text-lime-500 md:text-black" />
+        We've got the skills. Let's team up.
       </h1>
       <p className="my-5 text-xl">
         Tell us more about yourself and what's on your mind.
       </p>
-      <div className="flex w-full flex-col gap-3">
+      <form action="https://docs.google.com/forms/d/e/1FAIpQLSeISuULYy8xpdtFjGNZ_W5gOgZBkIKmyPCIMQLo2JPRdYdV8Q/formResponse" className="flex w-full flex-col gap-3">
         <input
           type="text"
-          name="fullname"
+          name="entry.2064855694"
           id="fullname"
           placeholder="Your name"
           className="w-full border-b border-zinc-600 p-1 placeholder-stone-600 md:bg-lime-400"
         />
         <input
           type="email"
-          name="email"
+          name="entry.464797640"
           id="email"
           placeholder="your@company.com"
           className="w-full border-b border-zinc-600 p-1 placeholder-stone-600 md:bg-lime-400"
         />
         <input
           type="text"
-          name="message"
+          name="entry.251431214"
           id="message"
           placeholder="Tell us a little about your project..."
           className="h-24 w-full border-b border-zinc-600 p-1 placeholder-stone-600 md:bg-lime-400"
@@ -49,7 +52,8 @@ const Form = () => {
               <label className="flex items-center gap-1" key={service}>
                 <input
                   type="checkbox"
-                  name="Website Design"
+                  name="entry.471991504"
+                  value={service}
                   className="size-5"
                 />{" "}
                 {service}
@@ -58,10 +62,11 @@ const Form = () => {
           })}
         </div>
 
-        <button className="rounded-lg bg-stone-950 p-1 text-white">
-          Let's get started
+        <button className="flex items-center justify-center gap-2 rounded-lg bg-stone-950 p-1 text-white">
+          Let's get started{" "}
+          <RiSparkling2Fill className="text-xl text-lime-400" />
         </button>
-      </div>
+      </form>
     </div>
   );
 };
---
+++ src/components/Untitled-1.md
@@ -0,0 +1,15 @@
+https://docs.google.com/forms/d/e/1FAIpQLSeISuULYy8xpdtFjGNZ_W5gOgZBkIKmyPCIMQLo2JPRdYdV8Q/formResponse
+
+usp=pp_url
+entry.2064855694
+entry.464797640
+entry.471991504
+
+https://docs.google.com/forms/d/e/1FAIpQLSeISuULYy8xpdtFjGNZ_W5gOgZBkIKmyPCIMQLo2JPRdYdV8Q/formResponse
+
+
+usp=pp_url
+entry.2064855694
+entry.464797640
+entry.251431214
+entry.471991504
